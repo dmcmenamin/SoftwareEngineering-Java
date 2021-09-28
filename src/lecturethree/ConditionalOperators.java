a41d1510f54a0986a236fdb45bbe4cf81b703b02
Darren McMenamin <mcmenamin.darren@gmail.com>
2021-09-28 18:41:10 +0100

    Initial Commit

---
+++ src/lecturethree/ConditionalOperators.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package lecturethree;
+
+/**
+ * @author Darren McMenamin
+ *
+ */
+public class ConditionalOperators {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1, num2, num3, num4;
+		num1 = 10;
+		num2 = 26;
+		num3 = 100;
+		num4 = 10 ;
+		
+		if (num1 == num4) {
+			System.out.println("Condition is true");
+		}
+		
+		System.out.println("End of Program");
+	}
+
+}
