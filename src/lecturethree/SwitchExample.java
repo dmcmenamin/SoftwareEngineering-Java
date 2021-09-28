6d52488c5aad6d5aff0686bcfda22dcecad5d180
Darren McMenamin <mcmenamin.darren@gmail.com>
2021-09-28 20:56:28 +0100

    Completed Lecture 3

---
+++ src/lecturethree/SwitchExample.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package lecturethree;
+
+/**
+ * @author Darren McMenamin
+ *
+ */
+public class SwitchExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 2;
+
+		switch (num1) {
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		case 3:
+			System.out.println("Three");
+			break;
+		default:
+			System.out.println("Unknown");
+		}
+
+	}
+
+}
